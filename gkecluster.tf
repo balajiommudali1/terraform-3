f7ee85e9d1c556d5ed6c5db1f153341cba603c77
Christopher Pearson <cpearson@doctorondemand.com>
2019-12-20 23:47:13 +0000

    working private cluster http://34.68.4.212:8080/

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,7 +1,7 @@
 resource "google_container_cluster" "mycluster" {
-  name               = "cptest15"
+  name               = "cptest16"
   network            = "default"
-  location               = "us-central1-a"
+  location           = "us-central1-a"
   initial_node_count = 3
   addons_config {
     http_load_balancing {
@@ -12,16 +12,16 @@ resource "google_container_cluster" "mycluster" {
       disabled = false
     }
   }
-master_authorized_networks_config {
-  cidr_blocks {
+  master_authorized_networks_config {
+    cidr_blocks {
       cidr_block   = "10.128.0.0/20"
       display_name = "azai"
     }
-}
+  }
 
-ip_allocation_policy  {
-cluster_ipv4_cidr_block  = "10.31.0.0/21"
-}
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block = "10.31.0.0/21"
+  }
   private_cluster_config {
     enable_private_endpoint = true
     enable_private_nodes    = true
@@ -35,25 +35,6 @@ provider "google" {
   project     = "it-sec-gcp-training"
   region      = "us-central1"
 }
-
-
-# resource "google_compute_network" "nwrk" {
-#   name                    = "test-network"
-#   auto_create_subnetworks = true
-# }
-# 
-# resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
-#   name          = "test-subnetwork"
-#   ip_cidr_range = "10.2.0.0/16"
-#   region        = "us-central1"
-#   network       = google_compute_network.nwrk.self_link
-#   secondary_ip_range {
-#     range_name    = "tf-test-secondary-range-update1"
-#     ip_cidr_range = "192.168.10.0/24"
-#   }
-#}
-
-
 
 
 resource "kubernetes_pod" "nginx" {
@@ -102,34 +83,12 @@ data "google_client_config" "default" {}
 
 provider "kubernetes" {
   load_config_file = false
-  host = google_container_cluster.mycluster.endpoint
-  token = data.google_client_config.default.access_token
+  host             = google_container_cluster.mycluster.endpoint
+  token            = data.google_client_config.default.access_token
 
-  client_certificate = base64decode(google_container_cluster.mycluster.master_auth[0].client_certificate)
-  client_key = base64decode(google_container_cluster.mycluster.master_auth[0].client_key)
+  client_certificate     = base64decode(google_container_cluster.mycluster.master_auth[0].client_certificate)
+  client_key             = base64decode(google_container_cluster.mycluster.master_auth[0].client_key)
   cluster_ca_certificate = base64decode(google_container_cluster.mycluster.master_auth[0].cluster_ca_certificate)
 }
 
 
-
-#resource "google_compute_instance" "shell" {
-#  name         = "shell"
-#    machine_type = "n1-standard-1"
-#	  zone         = "us-central1-a"
-#
-#
-#  boot_disk {
-#      initialize_params {
-#	        image = "centos-cloud/centos-8"
-#			    }
-#				  }
-#
-#  network_interface {
-#    network = "default"
-#
-#    access_config {
-#      // Ephemeral IP
-#    }
-#  }
-#
-#	  }
